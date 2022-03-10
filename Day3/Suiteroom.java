a623f5a393b05e2af9089ecc9b251be7876f6e63
sabarish-sab <98872619+sabarish-sab@users.noreply.github.com>
2022-03-10 09:02:50 +0530

    Create Suiteroom.java

---
+++ Day3/Suiteroom.java
@@ -0,0 +1,36 @@
+class Suiteroom extends Hotelroom{
+
+private int ratepersqfeet=15;
+Suiteroom()
+{
+}
+Suiteroom(int ratepersqfeet)
+{
+this.ratepersqfeet=15;
+}
+
+
+public void setRatepersqfeet(int sq)
+{
+this.ratepersqfeet=sq;
+}
+public int getRatepersqfeet()
+{
+return this.ratepersqfeet;
+}
+public  int display123(int a)
+{
+if(a==1)
+{
+
+a=ratepersqfeet+2;
+
+return a;
+}
+else
+{
+a=ratepersqfeet;
+return a;
+}
+}
+}
