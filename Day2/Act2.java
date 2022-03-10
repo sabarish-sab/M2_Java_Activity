ab7f8836b092869fc00f4c26ec97d5ea521e3290
sabarish-sab <98872619+sabarish-sab@users.noreply.github.com>
2022-03-10 08:52:33 +0530

    Create Act2.java

---
+++ Day2/Act2.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner14;
+
+import java.io.*;
+
+ public class Scorecard
+{
+private String teamname;
+private String innings;
+private int  score;
+
+public void setTeamname(String team)
+{
+this.teamname=team;
+}
+
+public String getTeamname(){
+return this.teamname;
+}
+public void setInnings(String in)
+{
+this.innings=in;
+}
+public String getInnings(){
+return this.innings;
+}
+public void setScore(int n)
+{
+this.score=n;
+}
+public int getScore()
+{
+return this.score;
+}
+ 
+public void display(){
+
+if(innings.equals("First"))
+{
+System.out.println("Name: "+teamname);
+System.out.println("Scored: "+score);
+System.out.println("Need "+score+"+1 to win the match");
+}
+else if(innings.equals("Second"))
+{
+System.out.println("Name: "+teamname);
+System.out.println("Scored: "+score);
+System.out.println("Match ends");
+}
+else
+{
+System.out.println("Invalid Input");
+}
+}
+}
